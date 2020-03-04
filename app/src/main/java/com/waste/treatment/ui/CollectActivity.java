f877ae4da2ff9b77ddb9288e3ade266565c4311b
cay <d>
2020-03-04 18:48:01 +0800

    第一次

--- app/src/main/java/com/waste/treatment/ui/CollectActivity.java
+++ app/src/main/java/com/waste/treatment/ui/CollectActivity.java
@@ -1,49 +1,92 @@
 package com.waste.treatment.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.waste.treatment.R;
-import com.waste.treatment.databinding.ActivityGenerateBinding;
-import com.waste.treatment.qrcode.BGAQRCodeUtil;
-import com.waste.treatment.qrcode.QRCodeEncoder;
+import com.waste.treatment.WasteTreatmentApplication;
+import com.waste.treatment.databinding.ActivityCollectBinding;
+import com.waste.treatment.util.Utils;
+import com.wildma.pictureselector.PictureSelector;
 
 public class CollectActivity extends AppCompatActivity {
-    private ActivityGenerateBinding mBinding;
+    private ActivityCollectBinding mBinding;
+    final static int TYPE_ALL = 0;
+    final static int TYPE_IMAGE = 1;
+    final static int TYPE_VIDEO = 2;
+    final static int TYPE_AUDIO = 3;
+    final static int CHOOSE_REQUEST = 188;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_generate);
-        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_generate);
-        mBinding.qrBtn.setOnClickListener(new View.OnClickListener() {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        Utils.makeStatusBarTransparent(this);
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_collect);
+        mBinding.ilTitle.tvTitle.setText(getResources().getString(R.string.collect_title));
+        mBinding.ilTitle.ivBack.setVisibility(View.VISIBLE);
+        mBinding.ilTitle.ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // createChineseQRCode(mBinding.qrBtn.getText().toString());
-                mBinding.qrImg.setImageBitmap(QRCodeEncoder.syncEncodeQRCode(mBinding.qrEdt.getText().toString(), BGAQRCodeUtil.dp2px(CollectActivity.this, 150)));
+                finish();
+            }
+        });
+        mBinding.imgBtnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PictureSelector.create(CollectActivity.this,PictureSelector.SELECT_REQUEST_CODE).selectPicture(false, 200, 200, 1, 1);;
 
             }
         });
+        mBinding.imgBtnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delImg(false,null);
+            }
+        });
+
 
     }
-  /*  @SuppressLint("StaticFieldLeak")
-    private void createChineseQRCode(final String s){
-        new AsyncTask<Void, Void, Bitmap>() {
-            @Override
-            protected Bitmap doInBackground(Void... params) {
-                return QRCodeEncoder.syncEncodeQRCode(s, BGAQRCodeUtil.dp2px(GenerateActivity.this, 150));
-            }
 
-            @Override
-            protected void onPostExecute(Bitmap bitmap) {
-                if (bitmap != null) {
-                    mBinding.qrImg.setImageBitmap(bitmap);
-                } else {
-                    Toast.makeText(GenerateActivity.this, "生成中文二维码失败", Toast.LENGTH_SHORT).show();
-                }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PictureSelector.SELECT_REQUEST_CODE){
+            if (data!=null){
+                String picturePath = data.getStringExtra(PictureSelector.PICTURE_PATH);
+               /* RequestOptions requestOptions = RequestOptions
+                        .circleCropTransform()
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true);*/
+                //Glide.with(this).load(picturePath)/*.apply(requestOptions)*/.into(mBinding.imgPhoto);
+                delImg(true,picturePath);
             }
-        }.execute();
-    }*/
+        }
+    }
+
+    private void delImg( Boolean bool ,String picturePath){
+        if (bool){
+            mBinding.imgBtnDel.setVisibility(View.VISIBLE);
+            mBinding.imgBtnPhoto.setVisibility(View.INVISIBLE);
+            Glide.with(this).load(picturePath)/*.apply(requestOptions)*/.into(mBinding.imgPhoto);
+
+        }else {
+            mBinding.imgBtnDel.setVisibility(View.INVISIBLE);
+            mBinding.imgBtnPhoto.setVisibility(View.VISIBLE);
+            mBinding.imgPhoto.setImageBitmap(null);
+           // Glide.with(this).load(picturePath)/*.apply(requestOptions)*/.into(mBinding.imgPhoto);
+        }
+
+    }
+
 }
